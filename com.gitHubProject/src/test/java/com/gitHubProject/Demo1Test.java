35734ea97abbf83f2d7a02ca9cb702f6bd8e975f
NAVEEN PATIL <NAVEEN PATIL@DESKTOP-F9SN8P7>
2021-11-11 12:47:39 +0530

    user1 this is first commit

---
+++ com.gitHubProject/src/test/java/com/gitHubProject/Demo1Test.java
@@ -0,0 +1,13 @@
+package com.gitHubProject;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test 
+{
+	@Test
+	public void demoTest()
+	{
+		System.out.println("user1 first commit");
+	}
+
+}
