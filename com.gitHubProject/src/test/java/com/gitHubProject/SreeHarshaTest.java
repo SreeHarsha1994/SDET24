810d10095c2ca7a0ac5fd586e8f0cbca561f9874
SREEHARSH <SREEHARSH@LAPTOP-SO7U3JNL>
2021-11-11 14:32:33 +0530

    From SreeHarsha

---
+++ com.gitHubProject/src/test/java/com/gitHubProject/SreeHarshaTest.java
@@ -0,0 +1,12 @@
+package com.gitHubProject;
+
+import org.testng.annotations.Test;
+
+public class SreeHarshaTest {
+	@Test
+	public void testingGithubTest()
+	{
+		System.out.println("Coming from SreeHarsha");
+	}
+
+}
