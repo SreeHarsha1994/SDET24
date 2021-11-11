09ec67fef79d7ae29941bcb23a096a9977e89502
NAVEEN PATIL <NAVEEN PATIL@DESKTOP-F9SN8P7>
2021-11-11 13:46:44 +0530

    this is commit2 user1

---
+++ com.gitHubProject/src/test/java/com/gitHubProject/SampleTest.java
@@ -0,0 +1,12 @@
+package com.gitHubProject;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void sample()
+	{
+		System.out.println("this is user1 commit2");
+	}
+
+}
