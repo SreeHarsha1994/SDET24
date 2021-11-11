bd77b4877ed433e226cb61017609e117de01db6b
NAVEEN PATIL <NAVEEN PATIL@192.168.1.23>
2021-11-11 22:14:10 +0530

    Batch_TC-01

---
+++ com.gitHubProject/src/test/java/com/gitHubProject/NaveenBatchTest.java
@@ -0,0 +1,13 @@
+package com.gitHubProject;
+
+import org.testng.annotations.Test;
+
+public class NaveenBatchTest 
+{
+	@Test
+	public void naveenbatch()
+	{
+		System.out.println("this is batch-TC-01");
+	}
+}
+
