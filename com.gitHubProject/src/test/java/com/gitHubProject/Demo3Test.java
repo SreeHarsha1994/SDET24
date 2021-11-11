1edf76a3a689aab4f21c9084a19b18787dae11c9
NAVEEN PATIL <NAVEEN PATIL@DESKTOP-F9SN8P7>
2021-11-11 14:02:13 +0530

    this is user1 commit 3

---
+++ com.gitHubProject/src/test/java/com/gitHubProject/Demo3Test.java
@@ -0,0 +1,12 @@
+package com.gitHubProject;
+
+import org.testng.annotations.Test;
+
+public class Demo3Test 
+{
+	@Test
+	public void demo3Test()
+	{
+		System.out.println("this is user 1 commit3");
+	}
+}
